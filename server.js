2f3f5333c2cae0c4fbf5fc673b28cbd77a38d7b7
Charitha Reddy <charithareddy1601@gmail.com>
2026-04-25 11:57:11 +0530

    Initial Docker app

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const HOST = '0.0.0.0';
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Docker App 🚀');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
